a3595bbe78b9f5e010493317e2e4fd4f3ec8b00d
salmanshahid5 <salmanshahid0333@gmail.com>
2024-09-02 15:15:15 +0500

    props learn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
 
-function App() {
-  return <h1>Chai aur react || Salman Shahid</h1>
+function Card(props) {
+  console.log(props);
+  
+  return (
+    <div>
+      <h1>{props.PropName}</h1>
+    </div>
+  );
 }
+
+function App() {
+  const arr = ['one', 'two', 'three'];
+  return (
+    <div>
+      {arr.map((v) => (
+        <Card PropName={v} key={v} />
+      ))}
+    </div>
+  );
+}
+
 
 export default App;
